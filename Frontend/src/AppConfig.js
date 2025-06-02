2710bcdf676f29d7b0cb2f8eddaa31f9342121b4
Zymeth-PGB <phangiabaovt2003@gmail.com>
2025-06-02 23:01:10 +0700

    Add Backend and Frontend folder

---
+++ Frontend/src/AppConfig.js
@@ -0,0 +1,11 @@
+/** @format */
+
+export const AppConfig = {
+  apiUrl: "http://192.168.152.141:8080",
+  routerBase: "",
+};
+
+export const AppConfigAddress = {
+  apiUrl: "",
+  routerBase: "",
+};
